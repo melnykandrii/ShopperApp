0b58263f8228434785ce00603feffa539c896b0c
Andrii Melnyk <melnik.andrey@gmail.com>
2021-10-21 00:24:28 -0400

    split homepage to components, adding components styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Category from "../../components/categories/category.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Category />
+  </div>
+);
+
+export default HomePage;
