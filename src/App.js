5eb8129053599a8dba25c03d050dd592d3e203d6
Andrii Melnyk <melnik.andrey@gmail.com>
2021-10-21 14:36:37 -0400

    adding shoppage, collectionitempreview and collectionitemcomponent

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
-import { CategoryPage } from "./pages/categorypage/categorypage.component";
+import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={CategoryPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/categorypage/categorypage.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const CategoryPage = () => (
-  <div>
-    <h1>Category Page</h1>
-  </div>
-);
