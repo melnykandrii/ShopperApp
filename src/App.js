c01ea94250568c6be71a86eed1c777cd52f69263
Andrii Melnyk <melnik.andrey@gmail.com>
2021-10-21 21:47:15 -0400

    adding authentication page, custom-button, input, sign-in components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import Header from "./components/header/header.component";
+import { AuthenticationPage } from "./pages/authentication/authenticationpage.component";
 
 function App() {
   return (
     <div>
-      <Header  />
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route path="/signin" component={AuthenticationPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/authentication/authenticationpage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./authenticationpage.styles.scss";
+
+export const AuthenticationPage = () => (
+  <div className="authentication">
+    <SignIn />
+  </div>
+);
