80c03a66f1895d5101f99058ac43943daa3c3658
Andrii Melnyk <melnik.andrey@gmail.com>
2021-11-05 18:16:58 -0400

    adding cart icon, cart-component, redux cart state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component";
+import Cart from "../cart/cart.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -28,12 +30,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <Cart />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
