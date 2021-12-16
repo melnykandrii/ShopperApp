5ae19feb0604be1815ea357895d016f6d589b612
Andrii Melnyk <melnik.andrey@gmail.com>
2021-12-16 01:02:44 -0500

    created selectors and updated components collections-overview, collection.component, adding styles

--- src/components/collections/collections-overview/collections-overview.component.jsx
+++ src/components/collections/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../../redux/shop/shop.selector";
+import { selectCollectionForPriview } from "../../../redux/shop/shop.selector";
 import { CollectionPreview } from "../collection-preview/collectionpreview.component";
 import "./collections-overview.styles.scss";
 
@@ -14,7 +14,7 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionForPriview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
