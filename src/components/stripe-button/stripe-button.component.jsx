5857f9ec198d2e908d5eef3d7651f468096f6e47
Andrii Melnyk <melnik.andrey@gmail.com>
2021-12-20 21:04:52 -0500

    stripe payment env setup

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import ENV from "../../services/env";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableStripeKey =
-    "pk_test_51J2e5xDdstQY4q5oEj99kEUVRbImfFhZPlCRhrDaX36d7f3Tv17Pdu7npzwjgbmzOOc52fwMofVEkfLUCjZtn4Bl006Rq5q1XM";
+
   const onToken = (token) => {
     console.log(token);
     alert("Payments successful!");
@@ -22,7 +22,7 @@ const StripeCheckoutButton = ({ price }) => {
       panelLabel="Pay Now"
       allowRememberMe
       token={onToken}
-      stripeKey={publishableStripeKey}
+      stripeKey={ENV.publishableStripeKey}
     />
   );
 };
